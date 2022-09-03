d8796cf7f22e4769258063f797c6bb82be9c7f24
chanseon.park <chanseon.park@kongstudios.net>
2022-09-04 01:27:05 +0900

    관중 텍스쳐 애니메이션

---
+++ Assets/TextureAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimationController : MonoBehaviour
+{
+    public Texture2D[] textures;
+    public float interval = 0.1f;
+    public new Renderer renderer;
+    private IEnumerator Start()
+    {
+        renderer = GetComponent<Renderer>();
+        int index = 0;
+        while (true)
+        {
+            renderer.material.mainTexture = textures[index];
+            yield return new WaitForSeconds(interval);
+            index++;
+            if (index >= textures.Length)
+                index = 0;
+        }
+    }
+}
