496f9bc5bdc929eef5a23391aceda6226ba834ec
chanseon.park <chanseon.park@kongstudios.net>
2022-09-04 02:05:25 +0900

    상태에 따라 엔진소리 다르게 나게함

---
+++ Assets/KartMover.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartMover : MonoBehaviour
+{
+    public float power = 10000f;
+    public float rotate = 1f;
+    public Rigidbody rb;
+    public AudioClip startClip;
+    public AudioClip runClip;
+    public AudioSource engineSource;
+    public AudioSource sfxSource;
+    float force;
+    float lastForce;
+    private void FixedUpdate()
+    {
+        var angularVelocity = rb.angularVelocity;
+        angularVelocity.x = 0; // 앞뒤로 흔들리는 물리 제거
+        angularVelocity.z = 0; // 좌우로 흔들리는 물리 제거
+        rb.angularVelocity = angularVelocity;
+
+        float forwardMove = Input.GetAxis("Vertical");
+        force = forwardMove * power;
+        if (force != 0)
+        {
+            PlayFirstStartSound();
+            var velocity = rb.velocity;
+            velocity.x = 0;
+            velocity.z = 0;
+            rb.velocity = velocity;
+            var forward = transform.forward;
+            forward.y = 0;
+            rb.AddForce(forward * force);
+        }
+        engineSource.volume = forwardMove;
+
+        float rotate = Input.GetAxis("Horizontal") * this.rotate;
+        rb.AddRelativeTorque(0, rotate, 0, ForceMode.VelocityChange);
+        lastForce = force;
+    }
+
+    private void PlayFirstStartSound()
+    {
+        if (lastForce != 0)
+            return;
+        sfxSource.Stop();
+        sfxSource.clip = startClip;
+        sfxSource.Play();
+    }
+
+    public void OnStart()
+    {
+        enabled = true;
+        engineSource.clip = runClip;
+        engineSource.Play();
+    }
+}
---
+++ Assets/StageManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    public static StageManager instance;
+    private void Awake() => instance = this;
+
+    public Text countText;
+    public int remainTime = 3;
+    public bool isGameStart = false;
+    public KartMover kartMover;
+    IEnumerator Start()
+    {
+        kartMover.enabled = false;
+        var wait = new WaitForSeconds(1);
+        
+        while(remainTime > 0)
+        {
+            countText.text = remainTime.ToString();
+            remainTime--;
+            yield return wait;
+        }
+        countText.text = "Start!";
+        isGameStart = true;
+
+        yield return wait;
+        countText.gameObject.SetActive(false);
+
+        kartMover.OnStart();
+    }
+}
